28de9af8db92439c9596ff6d3d85f4d5516e1c9e
MaksimTitenko <mt@intechs.by>
2020-09-12 11:15:28 +0300

    Added renewal book

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='books'),
     path('book/<int:pk>', BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<str:pk>/renew/', RenewBookView.as_view(), name='renew-book-librarian')
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
+import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView, ListView, DetailView
 
+from catalog.forms import RenewBookForm
 from catalog.models import *
 
 
@@ -40,3 +43,25 @@ class LoanedBooksByUserListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status='o').order_by('due_back')
+
+class RenewBookView(LoginRequiredMixin, TemplateView):
+    template_name = 'catalog/book_renew_librarian.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RenewBookView, self).get_context_data(**kwargs)
+        context['bookinst'] = get_object_or_404(BookInstance, pk=kwargs.get('pk'))
+        context['form'] = RenewBookForm(initial={'renewal_date': date.today() + datetime.timedelta(weeks=3)})
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = RenewBookForm(self.request.POST or None)
+        book_inst = context['bookinst']
+
+        if form.is_valid():
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+            return redirect('my-borrowed')
+
+        context['form'] = form
+        return render(request, self.template_name, context)
